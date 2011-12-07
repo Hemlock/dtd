ef2fdd70a6dd1312b18bcee61ee802d2e5dddc82
Kelly Taylor <kellyctaylor@gmail.com>
2011-12-07 16:28:40 -0500

    Animations for ff and chrome.

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-window.onload = function() {
-    var as = document.getElementsByTagName('a');
-    for (var a, i=0; a = as[i]; i++) {
-        addListener(a, 'mouseover', setState);
-    }
-}
-
-function addListener(target, event, handler) {
-    target['on' + event] = function() {
-        handler.call(target);
-    }
-}
-
-function setState() {
-    console.log(this);
+function show(item) {
+    var main = document.getElementById('main');
+    main.className = item.className;
 }
