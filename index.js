afb32202af7ff8bf0a63388ceedd21067805096b
Hemlock <kellyctaylor@gmail.com>
2012-12-15 08:46:01 -0500

    New version

--- index.js
+++ index.js
@@ -1,124 +1,176 @@
-DTD = {
-    address: [100, 111, 117, 103, 64, 100, 111, 117, 103, 116, 97, 121, 108, 111, 114, 100, 101, 99, 111, 114, 97, 116, 105, 110, 103, 46, 99, 111, 109],
-    setup: function() {
-        this.setupLinks();
-        this.setupContacts();
-        this.getMain().className = 'about';
-    },
+// TODO: scaling doesn't work right for a tall narrow window
+// TODO: more images
 
-    setupLinks: function() {
-        var main = this.getMain();
-        var links = this.getLinks();
-        links.onclick = links.onmouseover = function(event) {
-            event = event || window.event;
-            var cls = (event.target || event.srcElement).parentNode.className;
-            switch (cls) {
-                case 'about':
-                case 'services':
-                case 'photos':
-                    main.className = cls;
-                    break;
-                default:
-                    break;
+var DTD = (function() {
+	function randomChars() {
+		var i = Math.floor(Math.random() * 10);
+		var chars = [];
+		while (i--) {
+			chars.push('&#' + (Math.floor(Math.random() * 26) + 97) + ';');
+		}
+		return chars.join('');
+	}
+	
+	function characterArray(array) {
+		var html = [];
+		for (var i=0, c; c=array[i]; i++) {
+			html.push('<span class="e">&#' + c + ';</span><span class="d">' + randomChars() + '</span>');		
+		}
+		return html.join('');
+	}
+	
+	return {
+		index: 0,
+		address: [100, 111, 117, 103, 64, 100, 111, 117, 103, 116, 97, 121, 108, 111, 114, 100, 101, 99, 111, 114, 97, 116, 105, 110, 103, 46, 99, 111, 109],
+		phone: [54, 49, 54, 46, 54, 51, 52, 46, 50, 49, 54, 49],
+		images: [
+			'6827737202_770cb00307_b.jpg',
+			'6827748384_e782f1e094_b.jpg',
+			'6827749728_28d4e0c9db_b.jpg', 
+			'6827751832_255db79cbe_b.jpg',
+			'6973851597_58b29689df_b.jpg'
+		],
+		
+		on: function(target, event, handler, scope) {
+			var boundHandler = function() {
+				handler.apply(scope, arguments);
+			};
+			
+			if (target.addEventListener) {
+				target.addEventListener(event, boundHandler, false);
+			} else if (target.attachEvent) {
+				target.attachEvent('on' + event, boundHandler);
+			}
+		},
+		
+		init: function() {
+			DTD.setupContacts();
+			DTD.renderImages();
+			DTD.setupAnimations();
+		},
+		
+		setupContacts: function() {
+			var container = document.getElementById('contact');
+			var email = document.createElement('h2');
+			email.innerHTML = 'Email: ' + characterArray(this.address);
+			email.className = 'email';
+			DTD.on(email, 'click', this.openEmail, this);
+			container.appendChild(email);
+			
+			var phone = document.createElement('h2');
+			phone.innerHTML = 'Phone: ' + characterArray(this.phone);
+			phone.className = 'phone';
+			container.appendChild(phone);
+		},
+		
+		renderImages: function() {
+			var container = document.getElementById('background');
+			while(container.firstChild) {
+				container.removeChild(container.firstChild);
+			}
+			
+			container.appendChild(this.getImage(this.index-1));
+			container.appendChild(this.getImage(this.index));
+			container.appendChild(this.getImage(this.index+1));
 
-            }
-        }
-    },
+			this.resize();
+		},
+		
+		setupAnimations: function() {
+			DTD.on(document.getElementById('left'), 'click', this.animateLeft, this);
+			DTD.on(document.getElementById('right'), 'click', this.animateRight, this);
+		},
+		
+		animateLeft: function() {
+			this.animate(-1);
+		},
+		
+		animateRight: function() {
+			this.animate(1);
+		},
+		
+		animate: function(direction) {
+			if (!this.animating) {
+				this.animating = true;
+				var body = document.body;
+				var width = body.offsetWidth;
+				var target = -width * direction;
+				var background = document.getElementById('background');
+                var images = background.getElementsByTagName('img');
+				this.tween = new TWEEN.Tween({ left: 0 })
+					.to({ left: target }, 300)
+					.easing(TWEEN.Easing.Exponential.InOut)
+					.onUpdate(function() {
+                        for (var i= 0,image; image = images[i]; i++){
+                            image.style.left = (this.left + ((i-1) * width)) + 'px';
+                        }
+					})
+					.onComplete(function() {
+						DTD.animating = false;
+						DTD.index = (DTD.index + direction + DTD.images.length) %DTD.images.length;						
+						DTD.renderImages();
+					})
+					.start();
 
-    setupContacts: function() {
-        var html = '<h4 style="clear:both;">Contact</h4>' +
-								'Phone: 616.634.2161' +
-								'<div class="email" onclick="DTD.openEmail();">Email: ';
-								
-        for (var i=0, c; c = this.address[i]; i++) {
-            html += '<span class="e">&#' + c + ';</span><span class="d">' + randomChars() + '</span>';
-        }
-        
-        html += '</div>';
-				var sections = this.getSections();
-				for (var i=0, section; section = sections[i]; i++) {
-					section.innerHTML += html;
+				this.animateStep();
+			}
+		},
+		
+		animateStep: function() {
+			if (DTD.animating) {
+				TWEEN.update();
+				requestAnimationFrame(DTD.animateStep);
+			}
+		},
+		
+		getImage: function(index) {
+			index = (index + this.images.length) % this.images.length;
+			var image = this.images[index];
+			if (typeof image == 'string') {
+				var src = 'images/' + image;
+				image = this.images[index] = new Image();
+				image.src = src;
+                DTD.on(image, 'load', this.resize, this);
+			}
+			return image;
+		},
+
+		openEmail: function() {
+			var str = '';
+			for (var i = 0, chr; chr = DTD.address[i]; i++) {
+				str += String.fromCharCode(chr);
+			}
+
+			location.href = 'mailto: ' + str;
+		},
+		
+		resize: function() {
+			var body = document.body;
+			var height = body.offsetHeight;
+			var width = body.offsetWidth;	
+			var background = document.getElementById('background');
+			var images = background.childNodes;
+
+			var i = images.length;
+			while (i--) {
+				var image = images[i];
+				var style = image.style;
+				style.height = '';
+				style.width = '';
+				
+				if (height/width >= image.offsetHeight/image.offsetWidth) {
+					style.height = height + 'px';
+				} else {
+					style.width = width + 'px';
 				}
 				
-        function randomChars() {
-            var i = Math.floor(Math.random() * 10);
-            var chars = [];
-            while (i--) {
-                chars.push('&#' + (Math.floor(Math.random() * 26) + 97) + ';');
-            }
-            return chars.join('');
-        }
-    },
-
-    openEmail: function() {
-        var str = '';
-        for (var i = 0, chr; chr = this.address[i]; i++) {
-            str += String.fromCharCode(chr);
-        }
-
-        location.href = 'mailto: ' + str;
-    },
-
-    simpleSelect: function(selector, parent) {
-        parent = parent || document;
-        if (parent.querySelectorAll) {
-            return this.toArray(parent.querySelectorAll(selector));
-
-        } else {
-            selector = selector.split('.');
-            var elements = parent.getElemenstByTagName(selector[0] || '*');
-            if (!selector[1]) {
-                return this.toArray(elements);
-            }
-
-            var matches = [];
-            var re = new RegExp('\\b' + selector[1] + '\\b', 'i');
-            for (var i = 0, item; item = elements[i]; i++) {
-                if (re.test(item.className)) {
-                    matches.push(item);
-                }
-            }
-            return matches;
-        }
-    },
-
-    toArray: function(list, start) {
-        start = start || 0;
-        try {
-            return Array.prototype.slice.call(list, start);
-        } catch (e) {
-            var array = [];
-            var length = list.length;
-            for (var i = start; i < length; i++) {
-                array.push(list[i]);
-            }
-            return array;
-        }
-    },
-
-    getMain: function() {
-        return document.getElementById('main');
-    },
-
-    getLinks: function() {
-        return document.getElementById('links');
-    },
-
-    getPhotos: function() {
-        return document.getElementById('photos');
-    },
-
-    getSections: function() {
-        return this.simpleSelect('.section');
-    },
+				style.left = ((i-1) * width) + 'px';
+				//style.zIndex = i;
+			}	
+		}
 		
-    getEmailNodes: function() {
-        return this.simpleSelect('.email');
-    }
-};
+	};
+})();
 
-
-window.onload = function() {
-    DTD.Photos.setup();
-    DTD.setup();
-};
+DTD.on(window, 'load', DTD.init, DTD);
+DTD.on(window, 'resize', DTD.resize, DTD);
